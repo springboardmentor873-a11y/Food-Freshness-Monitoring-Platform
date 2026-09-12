9d4790be171318032fa00d4b14309ffe02444f2b
sharaffsafiya28 <sharaffsafiya2005@gmail.com>
2026-09-12 15:24:08 +0530

    Delete frontend- directory

--- frontend-/src/mocks/user.js
+++
@@ -1,8 +0,0 @@
-export const CURRENT_USER = {
-  id: "usr_001",
-  name: "Aarav Sharma",
-  email: "aarav.sharma@freshai.dev",
-  role: "Retail Manager",
-  avatarInitials: "AS",
-  avatarColor: "bg-gradient-brand",
-};
